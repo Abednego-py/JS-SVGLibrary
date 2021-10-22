bbc71aa2f87d6bbcaa71685f5e8b361d263a508e
Abednego-py <71936420+Abednego-py@users.noreply.github.com>
2021-10-22 18:41:00 +0100

    completed module

--- js/sight.js
+++ js/sight.js
@@ -2,7 +2,7 @@ class SVGElement {
     constructor(type){
         this.type = type;
         this.namespace = 'http://www.w3.org/2000/svg'; 
-        this.node = document.createElementNS(this.namespace,type);
+        this.node = document.createElementNS(this.namespace,this.type);
         return this
     }
     attr(attrs){
@@ -14,14 +14,14 @@ class SVGElement {
     }
     append(element){
         const parent = typeof(element) === 'string'? document.querySelector(element):element.node; 
-        parent.append(this.node)
+        parent.appendChild(this.node)
         return this
     }
 }
 
 class Sight{
     constructor(selector,width,height){
-        this.svg = new SVGElement('svg')
+        this.svg = new SVGElement(svg)
         svg.attr({viewbox:`0 0 ${width} ${height}`})
         svg.append(selector)
     }
